6337ef4f885aaa3cdaa135100acc0247dcc5aacf
Richard Domander <rdomander@rvc.ac.uk>
2016-04-04 16:18:32 +0100

    Add Main class

---
+++ Legacy/bonej/src/main/java/org/bonej/Main.java
@@ -0,0 +1,14 @@
+package org.bonej;
+
+import ij.ImageJ;
+
+/**
+ * A class for launching ImageJ1 and testing BoneJ legacy
+ *
+ * @author Richard Domander
+ */
+public class Main {
+    public static void main(String... args) {
+        ImageJ.main( args );
+    }
+}
