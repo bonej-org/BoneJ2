a8e61351fc5eb9529de20fdfbf5ab2563a70e99e
Richard Domander <rdomander@rvc.ac.uk>
2016-05-26 16:10:13 +0100

    Add Main class

---
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/BoneJMain.java
@@ -0,0 +1,14 @@
+package org.bonej.wrapperPlugins;
+
+import net.imagej.Main;
+
+/**
+ * A main class for quickly testing the wrapper plugins
+ *
+ * @author Richard Domander 
+ */
+public class BoneJMain {
+    public static void main(String... args) {
+        Main.launch(args);
+    }
+}
