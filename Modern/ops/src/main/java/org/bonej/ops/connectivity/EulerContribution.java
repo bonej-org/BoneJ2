b0ee737759c41c6a40d2ad64e1c53f0de01b2b3e
Richard Domander <rdomander@rvc.ac.uk>
2016-06-20 16:55:14 +0100

    Add tests

--- Modern/ops/src/main/java/org/bonej/ops/connectivity/EulerContribution.java
+++ Modern/ops/src/main/java/org/bonej/ops/connectivity/EulerContribution.java
@@ -11,7 +11,6 @@ import org.bonej.utilities.AxisUtils;
 import org.scijava.plugin.Plugin;
 
 import java.util.Optional;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 /**
@@ -40,9 +39,17 @@ public class EulerContribution<B extends BooleanType<B>> extends AbstractUnaryFu
     @Override
     public Double compute1(final ImgPlus<B> imgPlus) {
         final Traverser<B> traverser = new Traverser<>(imgPlus);
-        final int chiZero = cornerVertices(traverser);
+        final long chiZero = stackCorners(traverser);
+        final long e = stackEdges(traverser) + 3 * chiZero;
+        final long d = voxelVertices(traverser) + chiZero;
+        final long c = stackFaces(traverser) + 2 * e - 3 * chiZero;
+        final long b = voxelEdges(traverser);
+        final long a = voxelFaces(traverser);
 
-        return chiZero / 8.0;
+        final long chiOne = d - e;
+        final long chiTwo = a - b + c;
+
+        return chiTwo / 2.0 + chiOne / 4.0 + chiZero / 8.0;
     }
 
     /**
@@ -51,7 +58,7 @@ public class EulerContribution<B extends BooleanType<B>> extends AbstractUnaryFu
      * Calculates χ_0 from Odgaard and Gundersen
      * @implNote Public and static for testing purposes
      */
-    public static <B extends BooleanType<B>> int cornerVertices(final Traverser<B> traverser) {
+    public static <B extends BooleanType<B>> int stackCorners(final Traverser<B> traverser) {
         int foregroundVoxels = 0;
         foregroundVoxels += getAtLocation(traverser, traverser.x0, traverser.y0, traverser.z0);
         foregroundVoxels += getAtLocation(traverser, traverser.x1, traverser.y0, traverser.z0);
@@ -68,6 +75,7 @@ public class EulerContribution<B extends BooleanType<B>> extends AbstractUnaryFu
      * Count the foreground voxels on the edges lining the stack
      * <p>
      * Contributes to χ_1 from Odgaard and Gundersen
+     * @implNote Public and static for testing purposes
      */
     public static <B extends BooleanType<B>> long stackEdges(final Traverser<B> traverser) {
         final long[] foregroundVoxels = {0};
@@ -75,14 +83,217 @@ public class EulerContribution<B extends BooleanType<B>> extends AbstractUnaryFu
         // left to right stack edges
         LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
             LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
-                for (long x = 1; x < traverser.xSize; x++) {
+                for (long x = 1; x < traverser.x1; x++) {
                     foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
                 }
             });
         });
 
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+                for (long y = 1; y < traverser.y1; y++) {
+                    foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
+                }
+            });
+        });
+
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+                for (long z = 1; z < traverser.z1; z++) {
+                    foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
+                }
+            });
+        });
 
         return foregroundVoxels[0];
+    }
+
+    /**
+     * Count the foreground voxels on the faces that line the edges of the stack
+     * <p>
+     * Contributes to χ_2 from Odgaard and Gundersen
+     * @implNote Public and static for testing purposes
+     */
+    public static <B extends BooleanType<B>> int stackFaces(final Traverser<B> traverser) {
+        final int[] foregroundVoxels = {0};
+
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            for (int y = 1; y < traverser.y1; y++) {
+                for (int x = 1; x < traverser.x1; x++) {
+                    foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
+                }
+            }
+        });
+
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            for (int z = 1; z < traverser.z1; z++) {
+                for (int x = 1; x < traverser.x1; x++) {
+                    foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
+                }
+            }
+        });
+
+        LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+            for (int y = 1; y < traverser.y1; y++) {
+                for (int z = 1; z < traverser.z1; z++) {
+                    foregroundVoxels[0] += getAtLocation(traverser, x, y, z);
+                }
+            }
+        });
+
+        return foregroundVoxels[0];
+    }
+
+    public static <B extends BooleanType<B>> long voxelVertices(final Traverser<B> traverser) {
+        final int[] voxelVertices = {0};
+
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            traverser.access.setPosition(z, traverser.zIndex);
+            LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+                traverser.access.setPosition(y, traverser.yIndex);
+                for (long x = 1; x < traverser.xSize; x++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, x, traverser.xIndex)) {
+                        voxelVertices[0]++;
+                    }
+                }
+            });
+        });
+
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            traverser.access.setPosition(z, traverser.zIndex);
+            LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+                traverser.access.setPosition(x, traverser.xIndex);
+                for (long y = 1; y < traverser.ySize; y++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, y, traverser.yIndex)) {
+                        voxelVertices[0]++;
+                    }
+                }
+            });
+        });
+
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            traverser.access.setPosition(y, traverser.yIndex);
+            LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+                traverser.access.setPosition(x, traverser.xIndex);
+                for (long z = 1; z < traverser.zSize; z++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, z, traverser.zIndex)) {
+                        voxelVertices[0]++;
+                    }
+                }
+            });
+        });
+
+        return voxelVertices[0];
+    }
+
+    public static <B extends BooleanType<B>> long voxelEdges(final Traverser<B> traverser) {
+        final long[] voxelEdges = {0};
+
+        // Front and back faces (all 4 edges). Check 2 edges per voxel
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            traverser.access.setPosition(z, traverser.zIndex);
+            for (int y = 0; y <= traverser.ySize; y++) {
+                for (int x = 0; x <= traverser.xSize; x++) {
+                    final int voxel = getAtLocation(traverser, x, y, z);
+                    if (voxel > 0) {
+                        voxelEdges[0] += 2;
+                        continue;
+                    }
+
+                    voxelEdges[0] += getAtLocation(traverser, x, y - 1, z);
+                    voxelEdges[0] += getAtLocation(traverser, x - 1, y, z);
+                }
+            }
+        });
+
+        // Top and bottom faces (horizontal edges)
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            traverser.access.setPosition(y, traverser.yIndex);
+            for (int z = 1; z < traverser.zSize; z++) {
+                for (int x = 0; x < traverser.xSize; x++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, z, traverser.zIndex)) {
+                        voxelEdges[0]++;
+                    }
+                }
+            }
+        });
+
+        // Top and bottom faces (vertical edges)
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            traverser.access.setPosition(y, traverser.yIndex);
+            for (int z = 0; z < traverser.zSize; z++) {
+                for (int x = 0; x <= traverser.xSize; x++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, x, traverser.xIndex)) {
+                        voxelEdges[0]++;
+                    }
+                }
+            }
+        });
+
+        // Left and right faces (horizontal edges)
+        LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+            traverser.access.setPosition(x, traverser.xIndex);
+            for (int z = 1; z < traverser.zSize; z++) {
+                for (int y = 0; y < traverser.ySize; y++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, z, traverser.zIndex)) {
+                        voxelEdges[0]++;
+                    }
+                }
+            }
+        });
+
+        // Left and right faces (vertical edges)
+        LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+            traverser.access.setPosition(x, traverser.xIndex);
+            for (int z = 0; z < traverser.zSize; z++) {
+                for (int y = 1; y < traverser.ySize; y++) {
+                    if (isTwoNeighborhoodForeground(traverser.access, y, traverser.yIndex)) {
+                        voxelEdges[0]++;
+                    }
+                }
+            }
+        });
+
+        return voxelEdges[0];
+    }
+
+    public static <B extends BooleanType<B>> long voxelFaces(final Traverser<B> traverser) {
+        final long[] voxelFaces = {0};
+
+        LongStream.of(traverser.z0, traverser.z1).forEach(z -> {
+            traverser.access.setPosition(z, traverser.zIndex);
+            for (int y = 0; y <= traverser.ySize; y++) {
+                for (int x = 0; x <= traverser.xSize; x++) {
+                    if (isFourNeighborhoodForeground(traverser.access, x, traverser.xIndex, y, traverser.yIndex)) {
+                        voxelFaces[0]++;
+                    }
+                }
+            }
+        });
+
+        LongStream.of(traverser.x0, traverser.x1).forEach(x -> {
+            traverser.access.setPosition(x, traverser.xIndex);
+            for (int y = 0; y <= traverser.ySize; y++) {
+                for (int z = 1; z < traverser.zSize; z++) {
+                    if (isFourNeighborhoodForeground(traverser.access, y, traverser.yIndex, z, traverser.zIndex)) {
+                        voxelFaces[0]++;
+                    }
+                }
+            }
+        });
+
+        LongStream.of(traverser.y0, traverser.y1).forEach(y -> {
+            traverser.access.setPosition(y, traverser.yIndex);
+            for (int x = 1; x < traverser.ySize; x++) {
+                for (int z = 1; z < traverser.zSize; z++) {
+                    if (isFourNeighborhoodForeground(traverser.access, x, traverser.yIndex, z, traverser.zIndex)) {
+                        voxelFaces[0]++;
+                    }
+                }
+            }
+        });
+
+        return voxelFaces[0];
     }
 
     //region -- Helper methods --
@@ -92,6 +303,34 @@ public class EulerContribution<B extends BooleanType<B>> extends AbstractUnaryFu
         traverser.access.setPosition(y, traverser.yIndex);
         traverser.access.setPosition(z, traverser.zIndex);
         return (int) traverser.access.get().getRealDouble();
+    }
+
+    private static <B extends BooleanType<B>> boolean isTwoNeighborhoodForeground(final RandomAccess<B> access,
+            final long pos, final int dim) {
+        access.setPosition(pos, dim);
+        final boolean voxelA = access.get().get();
+        access.setPosition(pos - 1, dim);
+        final boolean voxelB = access.get().get();
+
+        return voxelA || voxelB;
+    }
+
+    private static <B extends BooleanType<B>> boolean isFourNeighborhoodForeground(final RandomAccess<B> access,
+            final long pos1, final int dim1, final long pos2, final int dim2) {
+        access.setPosition(pos1, dim1);
+        access.setPosition(pos2, dim2);
+        final boolean voxelA = access.get().get();
+        access.setPosition(pos1 - 1, dim1);
+        access.setPosition(pos2, dim2);
+        final boolean voxelB = access.get().get();
+        access.setPosition(pos1, dim1);
+        access.setPosition(pos2 - 1, dim2);
+        final boolean voxelC = access.get().get();
+        access.setPosition(pos1 - 1, dim1);
+        access.setPosition(pos2 - 1, dim2);
+        final boolean voxelD = access.get().get();
+
+        return voxelA || voxelB || voxelC || voxelD;
     }
     //endregion
 
--- Modern/ops/src/test/java/org/bonej/ops/connectivity/EulerContributionTest.java
+++ Modern/ops/src/test/java/org/bonej/ops/connectivity/EulerContributionTest.java
@@ -29,8 +29,23 @@ public class EulerContributionTest {
                 (ImgPlus<BitType>) IMAGE_J.op().run(Cuboid.class, null, 10, 10, 10, 1, 1, 5);
         final EulerContribution.Traverser<BitType> traverser = new EulerContribution.Traverser<>(cuboid);
 
-        final int cornerVertices = EulerContribution.cornerVertices(traverser);
-        assertEquals(0, cornerVertices);
+        final int vertices = EulerContribution.stackCorners(traverser);
+        assertEquals("Number of stack vertices is incorrect", 0, vertices);
+
+        final long edges = EulerContribution.stackEdges(traverser);
+        assertEquals("Number stack edge voxels is incorrect", 0, edges);
+
+        final int faces = EulerContribution.stackFaces(traverser);
+        assertEquals("Number stack face voxels is incorrect", 0, faces);
+
+        final long voxelVertices = EulerContribution.voxelVertices(traverser);
+        assertEquals("Number of voxel vertices is incorrect", 0, voxelVertices);
+
+        final long voxelFaces = EulerContribution.voxelFaces(traverser);
+        assertEquals("Number of voxel faces is incorrect", 0, voxelFaces);
+
+        final long voxelEdges = EulerContribution.voxelEdges(traverser);
+        assertEquals("Number of voxel edges is incorrect", 0, voxelEdges);
 
         final Double result = (Double) IMAGE_J.op().run(EulerContribution.class, cuboid);
         assertEquals("Euler contribution is incorrect", 0, result.intValue());
@@ -42,12 +57,34 @@ public class EulerContributionTest {
      */
     @Test
     public void testCompute1CuboidStackSize() throws Exception {
+        final int edges = 12;
+        final int cubeSize = 3;
+        final int edgeSize = cubeSize - 2;
         final ImgPlus<BitType> cuboid =
-                (ImgPlus<BitType>) IMAGE_J.op().run(Cuboid.class, null, 10, 10, 10, 1, 1, 0);
+                (ImgPlus<BitType>) IMAGE_J.op().run(Cuboid.class, null, cubeSize, cubeSize, cubeSize, 1, 1, 0);
         final EulerContribution.Traverser<BitType> traverser = new EulerContribution.Traverser<>(cuboid);
 
-        final int cornerVertices = EulerContribution.cornerVertices(traverser);
-        assertEquals(8, cornerVertices);
+        final int vertices = EulerContribution.stackCorners(traverser);
+        assertEquals("Number of stack vertices is incorrect", 8, vertices);
+
+        final long stackEdges = EulerContribution.stackEdges(traverser);
+        assertEquals("Number stack edge voxels is incorrect", edges * edgeSize, stackEdges);
+
+        final int faces = EulerContribution.stackFaces(traverser);
+        assertEquals("Number stack face voxels is incorrect", 6 * edgeSize * edgeSize, faces);
+
+        final long voxelVertices = EulerContribution.voxelVertices(traverser);
+        assertEquals("Number of voxel vertices is incorrect", edges * (cubeSize - 1), voxelVertices);
+
+        final long xyFace = (cubeSize + 1) * (cubeSize + 1);
+        final long yzFace = (cubeSize - 1) * (cubeSize + 1);
+        final long xzFace = (cubeSize - 1) * (cubeSize - 1);
+        final long expectedVoxelFaces = xyFace * 2 + yzFace * 2 + xzFace * 2;
+        final long voxelFaces = EulerContribution.voxelFaces(traverser);
+        assertEquals("Number of voxel faces is incorrect", expectedVoxelFaces, voxelFaces);
+
+        final long voxelEdges = EulerContribution.voxelEdges(traverser);
+        assertEquals("Number of voxel edges is incorrect", 108, voxelEdges);
 
         final Double result = (Double) IMAGE_J.op().run(EulerContribution.class, cuboid);
         assertEquals("Euler contribution is incorrect", 1, result.intValue());
