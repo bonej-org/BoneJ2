69d12b419b67c771e9908763434cf029f2115d90
rimadoma <rdomander@rvc.ac.uk>
2016-07-08 18:25:52 +0200

    Refactor SurfaceMask
    Change the op to operate on genaral RAIs and not care about input dimensionality

--- Modern/ops/src/main/java/org/bonej/ops/thresholdFraction/SurfaceMask.java
+++ Modern/ops/src/main/java/org/bonej/ops/thresholdFraction/SurfaceMask.java
@@ -1,17 +1,12 @@
 package org.bonej.ops.thresholdFraction;
 
-import net.imagej.ImgPlus;
-import net.imagej.ops.Contingent;
 import net.imagej.ops.Op;
 import net.imagej.ops.special.function.AbstractBinaryFunctionOp;
-import net.imglib2.Dimensions;
-import net.imglib2.FinalDimensions;
-import net.imglib2.RandomAccess;
+import net.imglib2.*;
 import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
-import org.bonej.utilities.AxisUtils;
 import org.scijava.plugin.Plugin;
 
 /**
@@ -22,43 +17,35 @@ import org.scijava.plugin.Plugin;
  */
 @Plugin(type = Op.class, name = "surfaceMask")
 public class SurfaceMask<T extends NativeType<T> & RealType<T>> extends
-        AbstractBinaryFunctionOp<ImgPlus<T>, Thresholds<T>, Img<BitType>> implements Contingent {
+        AbstractBinaryFunctionOp<RandomAccessibleInterval<T>, Thresholds<T>, Img<BitType>> {
 
-    /** Marching cubes is only defined for 3D images */
+    /**
+     * Creates a surface mask
+     *
+     * @param interval      A RAI where the first three dimensions are spatial
+     * @param thresholds    Thresholds whose type matches that of the elements in the interval
+     * @return              A three dimensional mask that can be given to a marching cubes op
+     */
     @Override
-    public boolean conforms() {
-        return AxisUtils.countSpatialDimensions(in1()) == 3;
-    }
-
-    @Override
-    public Img<BitType> compute2(final ImgPlus<T> imgPlus, final Thresholds<T> thresholds) {
-        final int[] indices = AxisUtils.getXYZIndices(imgPlus).get();
-        final int xIndex = indices[0];
-        final int yIndex = indices[1];
-        final int zIndex = indices[2];
-        final long width = imgPlus.dimension(xIndex);
-        final long height = imgPlus.dimension(yIndex);
-        final long depth = imgPlus.dimension(zIndex);
-
+    public Img<BitType> compute2(final RandomAccessibleInterval<T> interval, final Thresholds<T> thresholds) {
+        final long width = interval.dimension(0);
+        final long height = interval.dimension(1);
+        final long depth = interval.dimension(2);
         final Dimensions dimensions = new FinalDimensions(width, height, depth);
         final Img<BitType> mask = ops().create().img(dimensions, new BitType());
-        final RandomAccess<BitType> maskAccess = mask.randomAccess();
-        final RandomAccess<T> imgAccess = imgPlus.randomAccess();
+        final Cursor<BitType> cursor = mask.localizingCursor();
+        final RandomAccess<T> intervalAccess = interval.randomAccess();
+        final long[] maskPosition = new long[3];
+        final long[] intervalPosition = new long[interval.numDimensions()];
 
-        for (long z = 0; z < depth; z++) {
-            imgAccess.setPosition(z, zIndex);
-            maskAccess.setPosition(z, 2);
-            for (long y = 0; y < height; y++) {
-                imgAccess.setPosition(y, yIndex);
-                maskAccess.setPosition(y, 1);
-                for (int x = 0; x < width; x++) {
-                    imgAccess.setPosition(x, xIndex);
-                    maskAccess.setPosition(x, 0);
-                    final T element = imgAccess.get();
-                    if (element.compareTo(thresholds.min) >= 0 && element.compareTo(thresholds.max) <= 0) {
-                        maskAccess.get().setOne();
-                    }
-                }
+        while (cursor.hasNext()) {
+            cursor.fwd();
+            cursor.localize(maskPosition);
+            System.arraycopy(maskPosition, 0, intervalPosition, 0, 3);
+            intervalAccess.setPosition(intervalPosition);
+            final T intervalElement = intervalAccess.get();
+            if (intervalElement.compareTo(thresholds.min) >= 0 && intervalElement.compareTo(thresholds.max) <= 0) {
+                cursor.get().setOne();
             }
         }
 
