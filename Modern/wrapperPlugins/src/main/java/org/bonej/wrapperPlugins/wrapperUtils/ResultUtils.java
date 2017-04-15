67d3de13226a84b306bb5146e9a48f7da743f935
rimadoma <rdomander@rvc.ac.uk>
2017-04-15 14:28:54 +0100

    Add result utils

--- Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtils.java
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtils.java
@@ -1,14 +1,25 @@
 
 package org.bonej.wrapperPlugins.wrapperUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import net.imagej.axis.Axes;
+import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.axis.TypedAxis;
 import net.imagej.space.AnnotatedSpace;
+import net.imagej.table.GenericColumn;
+import net.imagej.table.LongColumn;
 import net.imagej.units.UnitService;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 
 import org.bonej.utilities.AxisUtils;
+import org.bonej.wrapperPlugins.wrapperUtils.HyperstackUtils.Subspace;
 
 /**
  * Static utility methods for displaying results to the user
@@ -93,8 +104,7 @@ public class ResultUtils {
 	 * @return Unit for column headers or empty if there's no unit
 	 */
 	public static <T extends AnnotatedSpace<CalibratedAxis>> String getUnitHeader(
-		final T space, final UnitService unitService,
-		final char exponent)
+		final T space, final UnitService unitService, final char exponent)
 	{
 		final Optional<String> unit = AxisUtils.getSpatialUnit(space, unitService);
 		if (!unit.isPresent()) {
@@ -110,5 +120,65 @@ public class ResultUtils {
 		}
 
 		return "(" + unitHeader + exponent + ")";
+	}
+
+	/**
+	 * Creates a column for a {@link net.imagej.table.GenericTable} that repeats
+	 * the given label on each row
+	 */
+	public static GenericColumn createLabelColumn(final String label, int rows) {
+		final GenericColumn labelColumn = new GenericColumn("Label");
+		final int n = Math.max(0, rows);
+		// TODO Replace with scijava-common/StringUtils.isNullOrEmpty
+		final String s = (label == null || label.isEmpty()) ? "-" : label;
+		Stream.generate(() -> s).limit(n).forEach(labelColumn::add);
+		return labelColumn;
+	}
+
+	/**
+	 * Creates columns for a {@link net.imagej.table.Table} that describe the
+	 * positions of the subspaces in a hyperspace
+	 * <p>
+	 * For example, if you've split a {X, Y, Z, C, T} space into {X, Y, Z}, the
+	 * method returns "Channel" and "Time" columns that show list the positions of
+	 * the subspaces in C and T.
+	 * </p>
+	 * 
+	 * @see Subspace
+	 */
+	public static <T extends RealType<T> & NativeType<T>> List<LongColumn>
+		createCoordinateColumns(List<Subspace<T>> subspaces)
+	{
+		final List<LongColumn> coordinateColumns = new ArrayList<>();
+		if (subspaces == null) {
+			return coordinateColumns;
+		}
+		final AxisType[] types = subspaces.get(0).getAxisTypes().toArray(
+			AxisType[]::new);
+		final List<long[]> positions = subspaces.stream().map(s -> s.getPosition()
+			.toArray()).collect(Collectors.toList());
+		for (int i = 0; i < types.length; i++) {
+			final AxisType type = types[i];
+			final LongColumn coordinateColumn = new LongColumn(type.getLabel());
+			final int index = i;
+			positions.stream().mapToLong(p -> toConventionalIndex(type, p[index]))
+				.forEach(coordinateColumn::add);
+			coordinateColumns.add(coordinateColumn);
+		}
+		return coordinateColumns;
+	}
+
+	/**
+	 * If needed, converts the given index to the ImageJ(1) convention where Z,
+	 * Channel and Time axes start from 1
+	 */
+	public static long toConventionalIndex(final AxisType type,
+		final long index)
+	{
+		final Stream<AxisType> oneAxes = Stream.of(Axes.Z, Axes.CHANNEL, Axes.TIME);
+		if (oneAxes.anyMatch(t -> t.equals(type))) {
+			return index + 1;
+		}
+		return index;
 	}
 }
--- Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtilsTest.java
+++ Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtilsTest.java
@@ -2,21 +2,31 @@
 package org.bonej.wrapperPlugins.wrapperUtils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import net.imagej.ImageJ;
 import net.imagej.ImgPlus;
 import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
 import net.imagej.axis.DefaultLinearAxis;
+import net.imagej.table.GenericColumn;
+import net.imagej.table.LongColumn;
 import net.imagej.units.UnitService;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.real.DoubleType;
 
+import org.bonej.wrapperPlugins.wrapperUtils.HyperstackUtils.Subspace;
 import org.junit.AfterClass;
 import org.junit.Test;
 
@@ -61,7 +71,7 @@ public class ResultUtilsTest {
 			'\u2076', '\u2077', '\u2078', '\u2079', '\u0000' };
 		final AxisType spatialAxis = Axes.get("Spatial", true);
 		final DefaultLinearAxis axis = new DefaultLinearAxis(spatialAxis);
-		final ImgPlus mockImage = mock(ImgPlus.class);
+		final ImgPlus<?> mockImage = mock(ImgPlus.class);
 		when(mockImage.axis(anyInt())).thenReturn(axis);
 
 		for (int i = 0; i < expected.length; i++) {
@@ -128,5 +138,99 @@ public class ResultUtilsTest {
 			exponent);
 
 		assertEquals("Unexpected unit header", "(" + unit + exponent + ")", result);
+	}
+
+	@Test
+	public void testCreateLabelColumnNullString() throws Exception {
+		final GenericColumn column = ResultUtils.createLabelColumn(null, 3);
+
+		assertNotNull(column);
+		assertTrue("Column has incorrect values", column.stream().map(
+			o -> (String) o).allMatch("-"::equals));
+	}
+
+	@Test
+	public void testCreateLabelColumnEmptyString() throws Exception {
+		final GenericColumn column = ResultUtils.createLabelColumn("", 3);
+
+		assertTrue("Column has incorrect values", column.stream().map(
+			o -> (String) o).allMatch("-"::equals));
+	}
+
+	@Test
+	public void testCreateLabelNegativeRows() throws Exception {
+		final GenericColumn column = ResultUtils.createLabelColumn("Test", -1);
+
+		assertEquals(0, column.size());
+	}
+
+	@Test
+	public void testCreateLabelColumn() throws Exception {
+		final String label = "Test";
+		final int rows = 10;
+
+		final GenericColumn column = ResultUtils.createLabelColumn(label, rows);
+
+		assertEquals("Column header is incorrect", "Label", column.getHeader());
+		assertEquals("Wrong number of rows", rows, column.size());
+		assertTrue("Column has incorrect values", column.stream().map(
+			o -> (String) o).allMatch(label::equals));
+	}
+
+	@Test
+	public void testCreateCoordinateColumnsNullSubspace() throws Exception {
+		final List<LongColumn> columns = ResultUtils.createCoordinateColumns(null);
+
+		assertNotNull(columns);
+		assertTrue(columns.isEmpty());
+	}
+
+	@Test
+	public void testCreateCoordinateColumns() throws Exception {
+		// SETUP
+		final Iterator<Long> expectedCPositions = Stream.of(1L, 2L, 1L, 2L)
+			.iterator();
+		final Iterator<Long> expectedTPositions = Stream.of(1L, 1L, 2L, 2L)
+			.iterator();
+		final Img<BitType> img = ArrayImgs.bits(2, 2, 2, 2, 2);
+		final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "",
+			new DefaultLinearAxis(Axes.X), new DefaultLinearAxis(Axes.Y),
+			new DefaultLinearAxis(Axes.Z), new DefaultLinearAxis(Axes.CHANNEL),
+			new DefaultLinearAxis(Axes.TIME));
+		final List<Subspace<BitType>> subspaces = HyperstackUtils.split3DSubspaces(
+			imgPlus).collect(Collectors.toList());
+
+		// EXECUTE
+		final List<LongColumn> coordinateColumns = ResultUtils
+			.createCoordinateColumns(subspaces);
+
+		// VERIFY
+		assertEquals("Wrong number of columns", 2, coordinateColumns.size());
+		assertEquals("Incorrect header", Axes.CHANNEL.getLabel(), coordinateColumns
+			.get(0).getHeader());
+		assertEquals("Incorrect header", Axes.TIME.getLabel(), coordinateColumns
+			.get(1).getHeader());
+		assertTrue("Columns have wrong number of values", coordinateColumns.stream()
+			.allMatch(c -> c.size() == 4));
+		assertTrue("Wrong values in the channel column", coordinateColumns.get(0)
+			.stream().allMatch(p -> p.longValue() == expectedCPositions.next()));
+		assertTrue("Wrong values in the time column", coordinateColumns.get(1)
+			.stream().allMatch(p -> p.longValue() == expectedTPositions.next()));
+	}
+
+    @Test
+    public void testToConventionalIndexNullType() throws Exception {
+        assertEquals(0, ResultUtils.toConventionalIndex(null, 0));
+    }
+
+	@Test
+	public void testToConventionalIndex() throws Exception {
+		final Stream<AxisType> types = Stream.of(Axes.X, Axes.Y, Axes.Z,
+			Axes.CHANNEL, Axes.TIME);
+		final Iterator<Long> expectedIndices = Stream.of(0L, 0L, 1L, 1L, 1L)
+			.iterator();
+
+		types.map(type -> ResultUtils.toConventionalIndex(type, 0)).forEach(
+			i -> assertEquals(expectedIndices.next().longValue(), i.longValue()));
 	}
 }
