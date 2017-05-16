1785416d6be72a287652a4d30389bca724562237
rimadoma <rdomander@rvc.ac.uk>
2017-05-16 12:38:28 +0100

    Fix Hamcrest clash
    Update Mockito version and calls to avoid Hamcrest clash that happens with
    ThreeDViewer

--- Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/ElementFractionWrapperTest.java
+++ Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/ElementFractionWrapperTest.java
@@ -4,8 +4,8 @@ package org.bonej.wrapperPlugins;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.after;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -115,6 +115,7 @@ public class ElementFractionWrapperTest {
 			expectedRatios };
 		final String[] expectedHeaders = { "Bone volume (" + unit + "³)",
 			"Total volume (" + unit + "³)", "Volume Ratio" };
+        @SuppressWarnings("unchecked")
 		final ImgPlus<BitType> imgPlus = (ImgPlus<BitType>) IMAGE_J.op().run(
 			Cuboid.class, cubeSide, cubeSide, cubeSide, 1, 1, padding, scale, unit);
 
--- Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtilsTest.java
+++ Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/wrapperUtils/ResultUtilsTest.java
@@ -4,7 +4,7 @@ package org.bonej.wrapperPlugins.wrapperUtils;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
