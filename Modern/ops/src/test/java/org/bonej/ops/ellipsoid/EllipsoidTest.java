23855e2b616a33fa383824864c35a83548e67f4e
rimadoma <rdomander@rvc.ac.uk>
2017-11-04 20:12:15 +0000

    Add centroid tests
    Add tests for the centroid field in Ellipsoid

--- Modern/ops/src/test/java/org/bonej/ops/ellipsoid/EllipsoidTest.java
+++ Modern/ops/src/test/java/org/bonej/ops/ellipsoid/EllipsoidTest.java
@@ -2,15 +2,18 @@
 package org.bonej.ops.ellipsoid;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import net.imagej.ImageJ;
 
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.scijava.vecmath.Vector3d;
-
-import java.util.List;
 
 /**
  * Tests for {@link Ellipsoid}.
@@ -27,7 +30,7 @@ public class EllipsoidTest {
 	}
 
 	@Test
-	public void testConstructorSorting() throws Exception {
+	public void testConstructor() throws Exception {
 		final double a = 1.0;
 		final double b = 2.0;
 		final double c = 3.0;
@@ -35,8 +38,12 @@ public class EllipsoidTest {
 		final Ellipsoid ellipsoid = new Ellipsoid(b, c, a);
 
 		assertEquals(a, ellipsoid.getA(), 1e-12);
-        assertEquals(b, ellipsoid.getB(), 1e-12);
-        assertEquals(c, ellipsoid.getC(), 1e-12);
+		assertEquals(b, ellipsoid.getB(), 1e-12);
+		assertEquals(c, ellipsoid.getC(), 1e-12);
+		final Vector3d centroid = ellipsoid.getCentroid();
+		assertNotNull("Default centroid should not be null", centroid);
+		assertEquals("Default centroid should be at origin", new Vector3d(0, 0, 0),
+			centroid);
 	}
 
 	@Test(expected = NullPointerException.class)
@@ -137,20 +144,67 @@ public class EllipsoidTest {
 	}
 
 	@Test
-    public void testSamplePoints() throws Exception {
-        final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3, imageJ.op());
-        final long n = 10;
+	public void testSamplePoints() throws Exception {
+		final Vector3d centroid = new Vector3d(4, 5, 6);
+		final double a = 2.0;
+        final double b = 3.0;
+		final double c = 4.0;
+		final Ellipsoid ellipsoid = new Ellipsoid(a, b, c, imageJ.op());
+		ellipsoid.setCentroid(centroid);
+		final long n = 10;
+		final Function<Vector3d, Double> ellipsoidEq = (Vector3d p) -> {
+			final BiFunction<Double, Double, Double> term = (x, r) -> (x * x) / (r *
+				r);
+			return term.apply(p.x, a) + term.apply(p.y, b) + term.apply(p.z, c);
+		};
 
-        final List<Vector3d> points = ellipsoid.samplePoints(n);
+		final List<Vector3d> points = ellipsoid.samplePoints(n);
 
-        assertNotNull(points);
-        assertEquals(n, points.size());
-    }
+		assertNotNull(points);
+		assertEquals(n, points.size());
+		points.forEach(p -> p.sub(centroid));
+		points.forEach(p -> assertEquals(
+			"Point doesn't solve the ellipsoid equation", 1.0, ellipsoidEq.apply(p),
+			1e-12));
+	}
 
-    @Test(expected = NullPointerException.class)
-    public void testSamplePointsThrowsNPEIfOpEnvironmentNull() throws Exception {
-        final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3);
+	@Test(expected = NullPointerException.class)
+	public void testSamplePointsThrowsNPEIfOpEnvironmentNull() throws Exception {
+		final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3);
 
-        ellipsoid.samplePoints(10);
-    }
+		ellipsoid.samplePoints(10);
+	}
+
+	@Test
+	public void testGetCentroid() {
+		final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3);
+		final Vector3d centroid = new Vector3d(6, 7, 8);
+		ellipsoid.setCentroid(centroid);
+
+		final Vector3d c = ellipsoid.getCentroid();
+		c.add(new Vector3d(1, 1, 1));
+
+		assertEquals("Getter should have returned a copy, not a reference",
+			centroid, ellipsoid.getCentroid());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testSetCentroidThrowsNPEIfCentroidNull() {
+		final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3);
+
+		ellipsoid.setCentroid(null);
+	}
+
+	@Test
+	public void testSetCentroid() {
+		final Ellipsoid ellipsoid = new Ellipsoid(1, 2, 3);
+		final Vector3d centroid = new Vector3d(6, 7, 8);
+
+		ellipsoid.setCentroid(centroid);
+
+		assertFalse("Setter should not copy reference", centroid == ellipsoid
+			.getCentroid());
+		assertEquals("Setter copied values wrong", centroid, ellipsoid
+			.getCentroid());
+	}
 }
