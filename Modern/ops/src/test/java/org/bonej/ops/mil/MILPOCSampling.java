56ffa79692e5a15ef00b85255c8282d633e29b7f
Richard Domander <rdomander@rvc.ac.uk>
2017-11-22 19:02:21 +0000

    Add proof of concept

--- Modern/ops/src/test/java/org/bonej/ops/mil/MILPOCSampling.java
+++ Modern/ops/src/test/java/org/bonej/ops/mil/MILPOCSampling.java
@@ -1,18 +1,11 @@
 
 package org.bonej.ops.mil;
 
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Stream.generate;
-
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 import net.imagej.ImageJ;
@@ -21,33 +14,36 @@ import net.imagej.axis.Axes;
 import net.imagej.axis.DefaultLinearAxis;
 import net.imagej.ops.special.function.BinaryFunctionOp;
 import net.imagej.ops.special.function.Functions;
+import net.imagej.ops.special.hybrid.BinaryHybridCFI1;
+import net.imagej.ops.special.hybrid.Hybrids;
 import net.imglib2.Interval;
-import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.ValuePair;
-import net.imglib2.view.Views;
 
-import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.bonej.ops.BoxIntersect;
 import org.bonej.ops.RotateAboutAxis;
 import org.scijava.vecmath.AxisAngle4d;
 import org.scijava.vecmath.Point3d;
+import org.scijava.vecmath.Tuple3d;
 import org.scijava.vecmath.Vector3d;
 
+// TODO Link op class to comment.
 /**
- * A small proof-of-concept that shows that a grid of vectors samples an image
- * uniformly.
+ * A small proof-of-concept that shows that a grid of directional vectors
+ * samples an image uniformly when rotated multiple times. A similar type of
+ * sampling is the first step in the MeanInterceptLengths op.
  *
  * @author Richard Domander
  */
-public class MeanInterceptLengthProofOfConcept {
+public class MILPOCSampling {
 
 	private static final ImageJ IMAGE_J = new ImageJ();
 	private static BinaryFunctionOp<ValuePair<Point3d, Vector3d>, Interval, Optional<ValuePair<DoubleType, DoubleType>>> boxIntersect;
+	private static BinaryHybridCFI1<Tuple3d, AxisAngle4d, Tuple3d> rotateOp;
 	private static Random random = new Random();
 
 	public static void main(String... args) throws ExecutionException,
@@ -56,21 +52,19 @@ public class MeanInterceptLengthProofOfConcept {
 		final int width = 100;
 		final int height = 100;
 		final int depth = 100;
+		final int rotations = 100;
+		final int bins = 100;
 		final Img<FloatType> img = ArrayImgs.floats(width, height, depth);
 		boxIntersect = (BinaryFunctionOp) Functions.binary(IMAGE_J.op(),
 			BoxIntersect.class, Optional.class, ValuePair.class, img);
-
-        for (int i = 0; i < 100; i++) {
-            final AxisAngle4d rotation = RotateAboutAxis.randomAxisAngle();
-            createSections(img, rotation, 100).forEach(s -> sample(img, s,
-                    1.0));
-        }
-
-
-        // sampleParallel(img, 300, 1.0, 100);
-
-		printStatistics(img);
-		
+		rotateOp = Hybrids.binaryCFI1(IMAGE_J.op(), RotateAboutAxis.class,
+			Tuple3d.class, new Vector3d(), new AxisAngle4d());
+		for (int i = 0; i < rotations; i++) {
+			final AxisAngle4d rotation = RotateAboutAxis.randomAxisAngle();
+			final LineGrid grid = createGrid(img, rotation);
+			final Stream<Section> sections = createSections(img, grid, bins);
+			sections.forEach(s -> sample(img, s, 1.0));
+		}
 		final ImgPlus<FloatType> image = new ImgPlus<>(img, "Sampling count",
 			new DefaultLinearAxis(Axes.X), new DefaultLinearAxis(Axes.Y),
 			new DefaultLinearAxis(Axes.Z));
@@ -78,30 +72,46 @@ public class MeanInterceptLengthProofOfConcept {
 		IMAGE_J.ui().show(image);
 	}
 
-	private static <I extends Interval> Stream<Section> createSections(
-		final I interval, final AxisAngle4d rotation, final long bins)
+	private static LineGrid createGrid(final Img<?> img,
+		final AxisAngle4d rotation)
 	{
-		final LineGrid grid = new LineGrid(interval);
+		final LineGrid grid = new LineGrid(img);
 		grid.setRandomGenerator(random);
-		grid.setRotation(rotation, IMAGE_J.op());
-		if (Math.random() > 0.5) {
-		    grid.flipPlanes();
-        }
+		grid.setRotation(rotateOp, rotation);
+		if (Math.random() <= 0.5) {
+			grid.randomReflection();
+		}
+		return grid;
+	}
+
+	private static Stream<Section> createSections(final Interval interval,
+		final LineGrid grid, final long bins)
+	{
 		return grid.lines(bins).map(l -> section(l, interval)).filter(
 			Objects::nonNull);
 	}
 
-	private static void printStatistics(final Img<FloatType> stack) {
-		final IterableInterval<FloatType> iterable = Views.flatIterable(stack);
-		final SummaryStatistics statistics = new SummaryStatistics();
-		iterable.cursor().forEachRemaining(i -> statistics.addValue(i.get()));
-		System.out.println(statistics.toString());
-	}
-
+	/**
+	 * Adds one the value of each image element accessed during sampling.
+	 * <p>
+	 * Proceeds along the given section in the image from Section#tMin to
+	 * Section#tMax. After each increment the section coordinates are floored to
+	 * the voxel grid.
+	 * </p>
+	 *
+	 * @param img a 3D image.
+	 * @param section a section of a line inside the image.
+	 * @param increment the scalar step between the sample positions. For example,
+	 *          an increment of 1.0 adds a vector of length 1.0 to the position.
+	 *          More formally, this is the value added to <em>t</em> in the line
+	 *          equation <b>a</b> + <em>t</em><b>v</b>.
+	 */
 	private static void sample(final Img<FloatType> img, final Section section,
 		final double increment)
 	{
 		final Vector3d startOffset = new Vector3d(section.direction);
+		// Add a random offset so that sampling doesn't always start from the same
+		// plane
 		final double offsetScale = random.nextDouble() * increment;
 		startOffset.scale(offsetScale);
 		final Vector3d samplePoint = new Vector3d(section.direction);
@@ -116,6 +126,7 @@ public class MeanInterceptLengthProofOfConcept {
 			increment)
 		{
 			samplePoint.get(coordinates);
+			// Assuming that coordinates are always non-negative
 			final long[] voxelCoordinates = Arrays.stream(coordinates).mapToLong(
 				c -> (long) c).toArray();
 			access.setPosition(voxelCoordinates);
@@ -124,26 +135,16 @@ public class MeanInterceptLengthProofOfConcept {
 		}
 	}
 
-	private static void sampleParallel(final Img<FloatType> img, final long lines,
-		final double increment, final int rotations) throws ExecutionException,
-		InterruptedException
-	{
-		final Runnable task = () -> {
-			final AxisAngle4d rotation = RotateAboutAxis.randomAxisAngle();
-			createSections(img, rotation, lines).forEach(s -> sample(img, s,
-				increment));
-		};
-		final ExecutorService executors = Executors.newFixedThreadPool(5);
-		final List<Future> futures = generate(() -> task).map(executors::submit)
-			.limit(rotations).collect(toList());
-		for (final Future future : futures) {
-			future.get();
-		}
-		executors.shutdown();
-	}
-
-	private static <I extends Interval> Section section(
-		final ValuePair<Point3d, Vector3d> line, final I interval)
+	/**
+	 * Maps the line to section in the interval.
+	 *
+	 * @param line an (origin, direction pair)
+	 * @param interval a 3D interval.
+	 * @return a {@link Section} in the interval, null if line doesn't intersect
+	 *         it.
+	 */
+	private static Section section(final ValuePair<Point3d, Vector3d> line,
+		final Interval interval)
 	{
 		final Optional<ValuePair<DoubleType, DoubleType>> result = boxIntersect
 			.calculate(line, interval);
@@ -156,6 +157,11 @@ public class MeanInterceptLengthProofOfConcept {
 		return new Section(line.a, line.b, tMin, tMax);
 	}
 
+	/**
+	 * Stores the origin <b>a</b> and direction <b>v</b> of a line, and the
+	 * scalars for <b>a</b> + <em>t<sub>1</sub></em><b>v</b> and <b>a</b> +
+	 * <em>t<sub>2</sub></em><b>v</b> where the line intersects an interval.
+	 */
 	private static class Section {
 
 		public final double tMin;
