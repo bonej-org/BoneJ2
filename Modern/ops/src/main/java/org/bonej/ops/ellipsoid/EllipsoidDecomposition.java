24c9fddf7d649bd110391577cbe97bbf9db8227c
alessandrofelder <afelder@rvc.ac.uk>
2018-01-05 15:24:08 +0000

    wip: quadrics

--- Modern/ops/src/main/java/org/bonej/ops/ellipsoid/EllipsoidDecomposition.java
+++ Modern/ops/src/main/java/org/bonej/ops/ellipsoid/EllipsoidDecomposition.java
@@ -4,16 +4,20 @@ import net.imagej.ops.Op;
 import net.imagej.ops.special.function.AbstractUnaryFunctionOp;
 import net.imglib2.util.ValuePair;
 import org.scijava.plugin.Plugin;
+import org.scijava.vecmath.Matrix3d;
+import org.scijava.vecmath.Matrix4d;
 import org.scijava.vecmath.Vector3d;
+import org.scijava.vecmath.Vector4d;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 /**
- * Tries to create an {@link Ellipsoid} decomposition from a list of vertex point with inward pointing normals
+ * Tries to create an {@link Ellipsoid} decomposition from a list of vertex points with inward pointing normals
  * <p>
  * The ellipsoid decomposition is drawn from the paper of Bischoff and Kobbelt, 2002
+ * The variable naming follows Bischoff and Kobbelt's nomenclature.
  * </p>
  *
  * @author Alessandro Felder
@@ -30,7 +34,49 @@ public class EllipsoidDecomposition extends AbstractUnaryFunctionOp<List<ValuePa
         return optimalRadii.stream().map(r -> new Ellipsoid(r,r,r)).collect(Collectors.toList());
     }
 
-    private double calculateOptimalRadius(final List<Vector3d> vertices, final ValuePair<Vector3d, Vector3d> vnp) {
+    public static Matrix4d getQuadric1(Vector3d sphereCentre, double radius){
+        Matrix3d identity = new Matrix3d();
+        identity.setIdentity();
+        Vector3d minusSphereCentre = new Vector3d(-sphereCentre.getX(), -sphereCentre.getY(), -sphereCentre.getZ());
+        Matrix4d quadric1 = new Matrix4d(identity,minusSphereCentre, 1.0);
+        quadric1.setRow(3, new Vector4d(minusSphereCentre));
+        quadric1.m33 = sphereCentre.lengthSquared()-radius*radius;
+        return quadric1;
+    }
+
+    public static Matrix4d getQuadric2(Vector3d p, Vector3d q, Vector3d np, Vector3d nq){
+        Matrix3d NRotation = new Matrix3d();
+
+        NRotation.m00 = -np.getX()*nq.getX();
+        NRotation.m01 = -np.getX()*nq.getY();
+        NRotation.m02 = -np.getX()*nq.getZ();
+
+        NRotation.m10 = -np.getY()*nq.getX();
+        NRotation.m11 = -np.getY()*nq.getY();
+        NRotation.m12 = -np.getY()*nq.getZ();
+
+        NRotation.m20 = -np.getZ()*nq.getX();
+        NRotation.m21 = -np.getZ()*nq.getY();
+        NRotation.m22 = -np.getZ()*nq.getZ();
+
+        Matrix3d NRotationTransposed = new Matrix3d();
+        NRotationTransposed.transpose(NRotation);
+        Matrix3d q2Rotation = new Matrix3d(NRotation);
+        q2Rotation.add(NRotationTransposed);
+        q2Rotation.mul(0.5);
+
+        double nqDotqHalf = nq.dot(q)/2.0;
+        double npDotpHalf = np.dot(p)/2.0;
+
+        Vector3d q2Translation = new Vector3d(np.getX()*nqDotqHalf+nq.getX()*npDotpHalf,np.getY()*nqDotqHalf+nq.getY()*npDotpHalf,np.getZ()*nqDotqHalf+nq.getZ()*npDotpHalf);
+
+        Matrix4d quadric2 = new Matrix4d(q2Rotation,q2Translation, 1.0);
+        quadric2.setRow(3, new Vector4d(q2Translation));
+        quadric2.m33 = -np.dot(p)*nq.dot(q);
+        return quadric2;
+    }
+
+    private static double calculateOptimalRadius(final List<Vector3d> vertices, final ValuePair<Vector3d, Vector3d> vnp) {
         final List<Double> positiveRadii = vertices.stream().map(v -> calculateRadius(v, vnp)).filter(r -> r > 0).collect(Collectors.toList());
         positiveRadii.sort(Double::compareTo);
         if(positiveRadii.size() > 0)
@@ -39,7 +85,7 @@ public class EllipsoidDecomposition extends AbstractUnaryFunctionOp<List<ValuePa
             return Double.NaN;
     }
 
-    private double calculateRadius(Vector3d x, ValuePair<Vector3d, Vector3d> vertexWithNormal)
+    private static double calculateRadius(Vector3d x, ValuePair<Vector3d, Vector3d> vertexWithNormal)
     {
         Vector3d xMinusP = new Vector3d(x.getX()-vertexWithNormal.getA().getX(), x.getY()-vertexWithNormal.getA().getY(), x.getZ()-vertexWithNormal.getA().getZ());
         double distanceSquared = xMinusP.lengthSquared();
--- Modern/ops/src/test/java/org/bonej/ops/ellipsoid/EllipsoidDecompositionTest.java
+++ Modern/ops/src/test/java/org/bonej/ops/ellipsoid/EllipsoidDecompositionTest.java
@@ -5,14 +5,19 @@ import net.imagej.ops.special.function.Functions;
 import net.imagej.ops.special.function.UnaryFunctionOp;
 import net.imglib2.util.ValuePair;
 import org.junit.Test;
+import org.scijava.vecmath.Matrix3d;
+import org.scijava.vecmath.Matrix4d;
 import org.scijava.vecmath.Vector3d;
+import org.scijava.vecmath.Vector4d;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+//lots of expected values calculated with pen and paper.
 public class EllipsoidDecompositionTest {
     private static final ImageJ IMAGE_J = new ImageJ();
 
@@ -46,6 +51,56 @@ public class EllipsoidDecompositionTest {
         assertEquals(3, ellipsoids.size());
         assertEquals(2, ellipsoids.stream().filter(e -> e.getA()==2.0).collect(Collectors.toList()).size());
         assertEquals(1, ellipsoids.stream().filter(e -> Math.abs(e.getA()-Math.sqrt(2.0))<1.0e-12).collect(Collectors.toList()).size());
+
+    }
+
+    @Test
+    public void testQuadric1() {
+        Vector3d sphereCentre = new Vector3d(3,4,5);
+        double radius = 7.77;
+
+        Matrix4d q1 = EllipsoidDecomposition.getQuadric1(sphereCentre,radius);
+
+        Matrix3d identity = new Matrix3d();
+        identity.setIdentity();
+        Matrix3d q1Rotation = new Matrix3d();
+        q1.getRotationScale(q1Rotation);
+        assertEquals(identity, q1Rotation);
+
+        Vector4d expected = new Vector4d(-3,-4,-5, 50-7.77*7.77);
+
+        Vector4d bottomRow = new Vector4d();
+        q1.getRow(3, bottomRow);
+        assertEquals(expected, bottomRow);
+
+        Vector4d rightColumn = new Vector4d();
+        q1.getColumn(3, rightColumn);
+        assertEquals(expected, rightColumn);
+
+    }
+
+    @Test
+    public void testQuadric2() {
+
+        Vector3d p = new Vector3d(4,4,1);
+        Vector3d q = new Vector3d(2,2,1);
+        Vector3d np = new Vector3d(-Math.sqrt(2)/2.0,-Math.sqrt(2)/2.0,0);
+        Vector3d nq = new Vector3d(Math.sqrt(2)/2.0,Math.sqrt(2)/2.0,0);
+
+
+        // @formatter:off
+        final Matrix4d expected = new Matrix4d(
+                0.5,0.5,0.0,-3.0,
+                0.5,0.5,0.0,-3.0,
+                0.0,0.0,0.0,0.0,
+                -3.0,-3.0,0.0,16.0
+        );
+        // @formatter:on
+
+        Matrix4d q2 = EllipsoidDecomposition.getQuadric2(p,q,np,nq);
+
+        assertTrue(q2.epsilonEquals(expected,1.0e-12));
+
 
     }
 }
