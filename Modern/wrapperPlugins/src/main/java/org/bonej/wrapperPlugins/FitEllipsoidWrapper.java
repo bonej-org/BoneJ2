8c69925c1be72c05e7fc42c5c79d0f311bd33dbe
Richard Domander <rdomander@rvc.ac.uk>
2018-09-10 15:12:44 +0100

    Add progress bar (Fit ellipsoid)

--- Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/FitEllipsoidWrapper.java
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/FitEllipsoidWrapper.java
@@ -108,9 +108,11 @@ public class FitEllipsoidWrapper extends ContextCommand {
 			return;
 		}
 		statusService.showStatus("Fit ellipsoid: solving ellipsoid equation");
+		statusService.showProgress(0, 2);
 		final Matrix4d quadric = (Matrix4d) opService.run(SolveQuadricEq.class,
 			points);
 		statusService.showStatus("Fit ellipsoid: determining ellipsoid parameters");
+		statusService.showProgress(1, 2);
 		@SuppressWarnings("unchecked")
 		final Optional<Ellipsoid> result = (Optional<Ellipsoid>) opService.run(
 			QuadricToEllipsoid.class, quadric);
