f8876fa9b8cd4972f6a4d11eb34dc1e4ed0ec22d
Richard Domander <rdomander@rvc.ac.uk>
2018-09-11 16:48:34 +0100

    Fix typos

--- Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/IntertrabecularAngleWrapper.java
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/IntertrabecularAngleWrapper.java
@@ -88,7 +88,7 @@ import sc.fiji.skeletonize3D.Skeletonize3D_;
  * @see sc.fiji.analyzeSkeleton.AnalyzeSkeleton_
  */
 
-@Plugin(type = Command.class, menuPath = "Plugins>BoneJ>Intertrabecular angles")
+@Plugin(type = Command.class, menuPath = "Plugins>BoneJ>Inter-trabecular angles")
 public class IntertrabecularAngleWrapper extends ContextCommand {
 
 	public static final String NO_RESULTS_MSG =
