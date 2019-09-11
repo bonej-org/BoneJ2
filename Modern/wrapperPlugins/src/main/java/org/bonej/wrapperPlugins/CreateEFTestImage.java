3d1b9658f451b3533c8e5817223ba83df55ad1ea
alessandrofelder <afelder@rvc.ac.uk>
2019-09-11 11:21:57 +0100

    more versatile test image creation

--- Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/CreateEFTestImage.java
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/CreateEFTestImage.java
@@ -19,13 +19,26 @@ import java.util.Random;
 @Plugin(type = Command.class, menuPath = "Plugins>BoneJ>Test images>Create EF test images")
 public class CreateEFTestImage extends ContextCommand {
     @Parameter
-    double shortestEllipsoidDimension = 10;
+    double shortestEllipsoidDimension = 5;
+
+    @Parameter
+    int imageDimension = 200;
 
     @Parameter
     int nSpheres = 2;
 
     @Parameter
+    int nRods = 2;
+
+    @Parameter
+    int nPlates = 2;
+
+    @Parameter
+    int nSurfboards = 2;
+
+    @Parameter
     int randomSeed = 23;
+
 
     @Parameter(type=ItemIO.OUTPUT)
     ImgPlus<UnsignedIntType> testImgPlus;
@@ -34,55 +47,156 @@ public class CreateEFTestImage extends ContextCommand {
     @Override
     public void run() {
         //create image
-        final ArrayImg testImage = ArrayImgs.unsignedBytes(200, 200, 200);
+        final ArrayImg testImage = ArrayImgs.unsignedBytes(imageDimension, imageDimension, imageDimension);
 
         //create seed points
         rng = new Random();
         rng.setSeed(randomSeed);
 
+        drawSpheres(testImage);
+        drawRods(testImage);
+        drawPlates(testImage);
+        drawSurfboards(testImage);
+        testImgPlus = new ImgPlus<>(testImage,"Test EF image");
+    }
+
+    private void drawSpheres(ArrayImg testImage) {
         final ArrayLocalizingCursor<UnsignedByteType> localizingCursor = testImage.localizingCursor();
-        for(int i = 0; i<nSpheres; i++)
-        {
-            final int x = rng.nextInt(199);
-            final int y = rng.nextInt(199);
-            final int z = rng.nextInt(199);
+        for(int i = 0; i<nSpheres; i++) {
+            final int x = rng.nextInt(imageDimension-1);
+            final int y = rng.nextInt(imageDimension-1);
+            final int z = rng.nextInt(imageDimension-1);
 
             localizingCursor.reset();
-            while(localizingCursor.hasNext())
-            {
+            while (localizingCursor.hasNext()) {
                 localizingCursor.fwd();
-                final long[] position = new long[3];
-                localizingCursor.localize(position);
+                if(localizingCursor.get().get()==0) {
+                    final long[] position = new long[3];
+                    localizingCursor.localize(position);
 
-                //figure out centre squared
-                double xc2 = position[0]-x+0.5;
-                xc2 = xc2*xc2;
+                    //figure out centre squared
+                    double xc2 = position[0] - x + 0.5;
+                    xc2 = xc2 * xc2;
 
-                double yc2 = position[1]-y+0.5;
-                yc2 = yc2*yc2;
+                    double yc2 = position[1] - y + 0.5;
+                    yc2 = yc2 * yc2;
 
-                double zc2 = position[2]-z+0.5;
-                zc2 = zc2*zc2;
+                    double zc2 = position[2] - z + 0.5;
+                    zc2 = zc2 * zc2;
 
-                double r2 = shortestEllipsoidDimension*shortestEllipsoidDimension;
+                    double r2 = shortestEllipsoidDimension * shortestEllipsoidDimension;
 
-                //sphere
-                if(xc2+yc2+zc2 <= 16*r2)
-                {
-                    localizingCursor.get().set(255);
-                }
-                //rod
-                else if(xc2/(100*r2)+yc2/r2+zc2/r2 <= 1)
-                {
-                    localizingCursor.get().set(255);
-                }
-                //plate
-                else if(xc2/r2+yc2/(100*r2)+zc2/(100*r2) <= 1)
-                {
-                    localizingCursor.get().set(255);
+                    //sphere
+                    if (xc2 + yc2 + zc2 <= 100 * r2) {
+                        localizingCursor.get().set(255);
+                    }
                 }
             }
         }
-        testImgPlus = new ImgPlus<>(testImage,"Test EF image");
+    }
+
+    private void drawRods(ArrayImg testImage) {
+        final ArrayLocalizingCursor<UnsignedByteType> localizingCursor = testImage.localizingCursor();
+        for(int i = 0; i<nRods; i++) {
+            final int x = rng.nextInt(imageDimension-1);
+            final int y = rng.nextInt(imageDimension-1);
+            final int z = rng.nextInt(imageDimension-1);
+
+            localizingCursor.reset();
+            while (localizingCursor.hasNext()) {
+                localizingCursor.fwd();
+                if(localizingCursor.get().get()==0) {
+                    final long[] position = new long[3];
+                    localizingCursor.localize(position);
+
+                    //figure out centre squared
+                    double xc2 = position[0] - x + 0.5;
+                    xc2 = xc2 * xc2;
+
+                    double yc2 = position[1] - y + 0.5;
+                    yc2 = yc2 * yc2;
+
+                    double zc2 = position[2] - z + 0.5;
+                    zc2 = zc2 * zc2;
+
+                    double r2 = shortestEllipsoidDimension * shortestEllipsoidDimension;
+
+                    //rod
+                    if (xc2 / (1000 * 1000 * r2) + yc2 / (r2) + zc2 / (r2) <= 1) {
+                        localizingCursor.get().set(255);
+                    }
+                }
+            }
+        }
+    }
+
+    private void drawPlates(ArrayImg testImage) {
+        final ArrayLocalizingCursor<UnsignedByteType> localizingCursor = testImage.localizingCursor();
+        for(int i = 0; i<nPlates; i++) {
+            final int x = rng.nextInt(imageDimension-1);
+            final int y = rng.nextInt(imageDimension-1);
+            final int z = rng.nextInt(imageDimension-1);
+
+            localizingCursor.reset();
+            while (localizingCursor.hasNext()) {
+                localizingCursor.fwd();
+                if(localizingCursor.get().get()==0) {
+                    final long[] position = new long[3];
+                    localizingCursor.localize(position);
+
+                    //figure out centre squared
+                    double xc2 = position[0] - x + 0.5;
+                    xc2 = xc2 * xc2;
+
+                    double yc2 = position[1] - y + 0.5;
+                    yc2 = yc2 * yc2;
+
+                    double zc2 = position[2] - z + 0.5;
+                    zc2 = zc2 * zc2;
+
+                    double r2 = shortestEllipsoidDimension * shortestEllipsoidDimension;
+
+                    //plate
+                    if (xc2 / (r2) + yc2 / (1000 * r2) + zc2 / (1000 * r2) <= 1) {
+                        localizingCursor.get().set(255);
+                    }
+                }
+            }
+        }
+    }
+
+    private void drawSurfboards(ArrayImg testImage) {
+        final ArrayLocalizingCursor<UnsignedByteType> localizingCursor = testImage.localizingCursor();
+        for(int i = 0; i<nSurfboards; i++) {
+            final int x = rng.nextInt(imageDimension-1);
+            final int y = rng.nextInt(imageDimension-1);
+            final int z = rng.nextInt(imageDimension-1);
+
+            localizingCursor.reset();
+            while (localizingCursor.hasNext()) {
+                localizingCursor.fwd();
+                if(localizingCursor.get().get()==0) {
+                    final long[] position = new long[3];
+                    localizingCursor.localize(position);
+
+                    //figure out centre squared
+                    double xc2 = position[0] - x + 0.5;
+                    xc2 = xc2 * xc2;
+
+                    double yc2 = position[1] - y + 0.5;
+                    yc2 = yc2 * yc2;
+
+                    double zc2 = position[2] - z + 0.5;
+                    zc2 = zc2 * zc2;
+
+                    double r2 = shortestEllipsoidDimension * shortestEllipsoidDimension;
+
+                    //plate
+                    if (xc2 / (100 * 100 * r2) + yc2 / (100 * r2) + zc2 / (r2) <= 1) {
+                        localizingCursor.get().set(255);
+                    }
+                }
+            }
+        }
     }
 }
