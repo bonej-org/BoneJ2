a8d6b076266f12bacb4b11b15d818e45504dc023
rimadoma <richard.domander@gmail.com>
2020-02-07 14:14:56 +0000

    Refactor Anisotropy
    * Enforces constant MIL vector base length in sampling
    * Separates sampling line generation from the MIL op. This makes testing
    more feasible. Previously tests had too much randomness, and were too
    brittle.

--- Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/AnisotropyWrapper.java
+++ Modern/wrapperPlugins/src/main/java/org/bonej/wrapperPlugins/AnisotropyWrapper.java
@@ -34,6 +34,7 @@ import static org.scijava.ui.DialogPrompt.OptionType.OK_CANCEL_OPTION;
 import static org.scijava.ui.DialogPrompt.Result.OK_OPTION;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -49,9 +50,12 @@ import java.util.function.Function;
 
 import net.imagej.ImgPlus;
 import net.imagej.ops.OpService;
+import net.imagej.ops.linalg.rotate.Rotate3d;
 import net.imagej.ops.special.function.BinaryFunctionOp;
 import net.imagej.ops.special.function.Functions;
 import net.imagej.ops.special.function.UnaryFunctionOp;
+import net.imagej.ops.special.hybrid.BinaryHybridCFI1;
+import net.imagej.ops.special.hybrid.Hybrids;
 import net.imagej.ops.stats.regression.leastSquares.Quadric;
 import net.imagej.units.UnitService;
 import net.imglib2.RandomAccessibleInterval;
@@ -63,7 +67,9 @@ import org.apache.commons.math3.random.RandomVectorGenerator;
 import org.apache.commons.math3.random.UnitSphereRandomVectorGenerator;
 import org.bonej.ops.ellipsoid.Ellipsoid;
 import org.bonej.ops.ellipsoid.QuadricToEllipsoid;
-import org.bonej.ops.mil.MILPlane;
+import org.bonej.ops.mil.ParallelLineGenerator;
+import org.bonej.ops.mil.ParallelLineMIL;
+import org.bonej.ops.mil.PlaneParallelLineGenerator;
 import org.bonej.utilities.AxisUtils;
 import org.bonej.utilities.ElementUtil;
 import org.bonej.utilities.SharedTable;
@@ -122,7 +128,7 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 	// with data at hand. Other data may need a different number.
 	private static final int DEFAULT_LINES = 100;
 	private static final double DEFAULT_INCREMENT = 2.3;
-	private static BinaryFunctionOp<RandomAccessibleInterval<BitType>, Quaterniondc, Vector3d> milOp;
+	private static BinaryFunctionOp<RandomAccessibleInterval<BitType>, ParallelLineGenerator, Vector3d> milOp;
 	private static UnaryFunctionOp<Matrix4dc, Optional<Ellipsoid>> quadricToEllipsoidOp;
 	private static UnaryFunctionOp<List<Vector3d>, Matrix4dc> solveQuadricOp;
 	private final Function<Ellipsoid, Double> degreeOfAnisotropy =
@@ -201,6 +207,8 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 	@Parameter
 	private CommandService commandService;
 	private static UsageReporter reporter;
+	private static BinaryHybridCFI1<Vector3d, Quaterniondc, Vector3d> rotateOp;
+	private double milLength;
 
 	@Override
 	public void run() {
@@ -209,12 +217,13 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 			inputImage);
 		final List<Subspace<BitType>> subspaces = HyperstackUtils.split3DSubspaces(
 			bitImgPlus).collect(toList());
-		matchOps(subspaces.get(0));
+		calculateMILLength(subspaces.get(0).interval);
+		matchOps(subspaces.get(0).interval);
 		final List<Ellipsoid> ellipsoids = new ArrayList<>();
 		for (int i = 0; i < subspaces.size(); i++) {
 			statusService.showStatus("Anisotropy: sampling subspace #" + (i + 1));
-			final Subspace<BitType> subspace = subspaces.get(i);
-			final Ellipsoid ellipsoid = milEllipsoid(subspace);
+			final RandomAccessibleInterval<BitType> interval = subspaces.get(i).interval;
+			final Ellipsoid ellipsoid = milEllipsoid(interval);
 			if (ellipsoid == null) {
 				return;
 			}
@@ -236,6 +245,14 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 			throw new NullPointerException("Reporter cannot be null");
 		}
 		AnisotropyWrapper.reporter = reporter;
+	}
+
+
+	private void calculateMILLength(final RandomAccessibleInterval<BitType> interval) {
+		final long[] dimensions = new long[interval.numDimensions()];
+		interval.dimensions(dimensions);
+		final long maxDim = Arrays.stream(dimensions).max().orElse(0L);
+		milLength = lines * lines * maxDim;
 	}
 
 	private void addResult(final Subspace<BitType> subspace,
@@ -301,9 +318,7 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 	}
 
 	@SuppressWarnings("unchecked")
-	private void matchOps(final Subspace<BitType> subspace) {
-		milOp = Functions.binary(opService, MILPlane.class, Vector3d.class,
-			subspace.interval, new Quaterniond(), lines, samplingIncrement);
+	private void matchOps(final RandomAccessibleInterval<BitType> interval) {
 		final List<Vector3d> tmpPoints = generate(Vector3d::new).limit(
 			Quadric.MIN_DATA).collect(toList());
 		solveQuadricOp = Functions.unary(opService, Quadric.class, Matrix4dc.class,
@@ -311,12 +326,18 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 		final Matrix4dc matchingMock = new Matrix4d();
 		quadricToEllipsoidOp = (UnaryFunctionOp) Functions.unary(opService,
 			QuadricToEllipsoid.class, Optional.class, matchingMock);
+		rotateOp = Hybrids.binaryCFI1(opService, Rotate3d.class, Vector3d.class,
+				new Vector3d(), new Quaterniond());
+		ParallelLineGenerator generator =
+				new PlaneParallelLineGenerator(interval, new Quaterniond(), rotateOp, lines);
+		milOp = Functions.binary(opService, ParallelLineMIL.class, Vector3d.class,
+				interval, generator, milLength, samplingIncrement);
 	}
 
-	private Ellipsoid milEllipsoid(final Subspace<BitType> subspace) {
+	private Ellipsoid milEllipsoid(final RandomAccessibleInterval<BitType> interval) {
 		final List<Vector3d> pointCloud;
 		try {
-			pointCloud = runDirectionsInParallel(subspace.interval);
+			pointCloud = runDirectionsInParallel(interval);
 			if (pointCloud.size() < Quadric.MIN_DATA) {
 				cancel("Anisotropy could not be calculated - too few points");
 				return null;
@@ -338,15 +359,13 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 		return null;
 	}
 
-	/**
-	 * Creates a random isotropically distributed quaternion.
-	 *
-	 * @return a (rotation) quaternion which can be used as a parameter for the
-	 *         op.
-	 */
-	private static synchronized Quaterniondc randomQuaternion() {
+	private Callable<Vector3d> createMILTask(final RandomAccessibleInterval<BitType> interval) {
+		// A random isotropically distributed quaternion
 		final double[] v = qGenerator.nextVector();
-		return new Quaterniond(v[0], v[1], v[2], v[3]);
+		final Quaterniond quaternion = new Quaterniond(v[0], v[1], v[2], v[3]);
+		final PlaneParallelLineGenerator generator =
+				new PlaneParallelLineGenerator(interval, quaternion, rotateOp, lines);
+		return () -> milOp.calculate(interval, generator);
 	}
 
 	private List<Vector3d> runDirectionsInParallel(
@@ -361,9 +380,7 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 		// but we need some kind of upper bound
 		final int nThreads = Math.max(5, cores);
 		final ExecutorService executor = Executors.newFixedThreadPool(nThreads);
-		final Callable<Vector3d> milTask = () -> milOp.calculate(interval,
-			randomQuaternion());
-		final List<Future<Vector3d>> futures = generate(() -> milTask).limit(
+		final List<Future<Vector3d>> futures = generate(() -> createMILTask(interval)).limit(
 			directions).map(executor::submit).collect(toList());
 		final List<Vector3d> pointCloud = Collections.synchronizedList(
 			new ArrayList<>(directions));
@@ -435,7 +452,7 @@ public class AnisotropyWrapper<T extends RealType<T> & NativeType<T>> extends
 	 * <p>
 	 * The method's here only to enable reproducible unit test.
 	 * </p>
-	 * 
+	 *
 	 * @param seed a seed number.
 	 * @see Random#setSeed
 	 */
