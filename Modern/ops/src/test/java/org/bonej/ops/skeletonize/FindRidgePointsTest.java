7958697cfdc86b3fe097b1259d9983e20cc783a4
Michael Doube <michael@doube.net>
2025-04-08 23:28:04 +0200

    Update copyright blurbs

--- Modern/ops/src/test/java/org/bonej/ops/skeletonize/FindRidgePointsTest.java
+++ Modern/ops/src/test/java/org/bonej/ops/skeletonize/FindRidgePointsTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Ops created for BoneJ2
  * %%
- * Copyright (C) 2015 - 2024 Michael Doube, BoneJ developers
+ * Copyright (C) 2015 - 2025 Michael Doube, BoneJ developers
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
