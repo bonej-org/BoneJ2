5da3040b8220e548573003edc46f5b70e33596e1
Michael Doube <michael@doube.net>
2025-04-08 14:46:02 +0200

    Update copyright blurbs

--- Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/SurfaceAreaWrapperTest.java
+++ Modern/wrapperPlugins/src/test/java/org/bonej/wrapperPlugins/SurfaceAreaWrapperTest.java
@@ -2,7 +2,7 @@
  * #%L
  * High-level BoneJ2 commands.
  * %%
- * Copyright (C) 2015 - 2024 Michael Doube, BoneJ developers
+ * Copyright (C) 2015 - 2025 Michael Doube, BoneJ developers
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
