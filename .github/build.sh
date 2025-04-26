e65607897a962c4d1e4dc9cbc6371d83ca63852c
Michael Doube <michael@doube.net>
2025-04-27 00:06:19 +0200

    Pom (#362)
    * Try injecting credentials with build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
+
+# Patch ci-build.sh to insert server credentials into Maven settings
+sed -i 's|</servers>|<server><id>github</id><username>\${env.GITHUB_ACTOR}</username><password>\${env.BONEJ_PLUS_REPO}</password></server></servers>|' ci-build.sh
+
 sh ci-build.sh
