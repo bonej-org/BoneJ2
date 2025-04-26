ded23f34570d6122532ccc094dcaa6c829c07638
Michael Doube <michael@doube.net>
2025-04-27 00:18:06 +0200

    Update build.sh (#363)

--- .github/build.sh
+++ .github/build.sh
@@ -2,6 +2,8 @@
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
 
 # Patch ci-build.sh to insert server credentials into Maven settings
-sed -i 's|</servers>|<server><id>github</id><username>\${env.GITHUB_ACTOR}</username><password>\${env.BONEJ_PLUS_REPO}</password></server></servers>|' ci-build.sh
+sed -i '/<servers>/a\
+<server><id>github</id><username>\${env.GITHUB_ACTOR}</username><password>\${env.BONEJ_PLUS_REPO}</password></server>
+' ci-build.sh
 
 sh ci-build.sh
