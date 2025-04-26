dac25d1619fd2073405091d9bbe5330e84a18779
Michael Doube <michael@doube.net>
2025-04-27 00:24:30 +0200

    Update build.sh (#364)

--- .github/build.sh
+++ .github/build.sh
@@ -6,4 +6,6 @@ sed -i '/<servers>/a\
 <server><id>github</id><username>\${env.GITHUB_ACTOR}</username><password>\${env.BONEJ_PLUS_REPO}</password></server>
 ' ci-build.sh
 
+cat ~/.m2/settings.xml
+
 sh ci-build.sh
